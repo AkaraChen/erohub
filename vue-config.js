2b6de83e645e061e5d68ea0d664b07fbdab47fff
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2021-09-10 06:48:03 +0800

    Update vue-config.js

--- vue-config.js
+++ vue-config.js
@@ -6,6 +6,7 @@ module.exports = {
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
         iconPaths: {
+            faviconSVG: 'img/icons/favicon.svg',
             favicon32: 'img/icons/favicon-32x32.png',
             favicon16: 'img/icons/favicon-16x16.png',
             appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
