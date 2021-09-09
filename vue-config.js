0d962a96b47b2012f26c11a0754bd71ff75b0c39
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2021-09-10 06:45:25 +0800

    pwa

---
+++ vue-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    pwa: {
+        name: 'Erohub',
+        themeColor: '#FFF5F6FA',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        iconPaths: {
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        },
+    },
+}
