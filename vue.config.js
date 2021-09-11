a22bb1724faab781d8cb88491e636a7c797ee698
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2021-09-11 18:37:05 +0800

    update pwa

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-    pwa: {
-        name: 'Erohub',
-        themeColor: '#F4F6FA',
-        background_color:'#F4F6FA',
-        msTileColor: '#F4F6FA',
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'white',
-        iconPaths: {
-            faviconSVG: 'img/icons/favicon.svg',
-            favicon32: 'img/icons/favicon-32x32.png',
-            favicon16: 'img/icons/favicon-16x16.png',
-            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
-            maskIcon: 'img/icons/safari-pinned-tab.svg',
-            msTileImage: 'img/icons/msapplication-icon-144x144.png'
-        },
+  pwa: {
+    name: 'Erohub',
+    themeColor: '#F5F6FA',
+    background_color: '#F4F6FA',
+    msTileColor: '#F4F6FA',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'white',
+    iconPaths: {
+      faviconSVG: 'img/icons/favicon.svg',
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
     },
+    manifestOptions: {
+      background_color: '#F5F6FA'
+    }
+  }
 }
