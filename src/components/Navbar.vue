95577ceeaa84e24b8fff28d1f1290758ab01f32b
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2021-09-12 12:49:37 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/#/page/1">Erohub</a>
+      <a class="navbar-brand" href="/">Erohub</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/#/page/1">首页</a>
+            <a class="nav-link active" aria-current="page" href="/">首页</a>
           </li>
         </ul>
       </div>
