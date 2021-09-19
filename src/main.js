3d665a865c824c4aa9636137a3ba4f91134e3c8c
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2021-09-19 17:53:43 +0800

    精简

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import './tabler.css'
+import './tabler'
 
-const app = createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
