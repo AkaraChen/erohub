5cce5b7b9faf1e87e1020d2802eb476ea285a3ae
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2021-09-20 01:01:53 +0800

    精简

--- vue.config.js
+++ vue.config.js
@@ -7,12 +7,12 @@ module.exports = {
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'white',
     iconPaths: {
-      faviconSVG: 'img/icons/favicon.svg',
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
-      maskIcon: 'img/icons/safari-pinned-tab.svg',
-      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+      faviconSVG: 'img/icons/favicon.ico',
+      favicon32: 'img/icons/icon.png',
+      favicon16: 'img/icons/icon.png',
+      appleTouchIcon: 'img/icons/icon.png',
+      maskIcon: 'img/icons/favicon.ico',
+      msTileImage: 'img/icons/icon.png'
     },
     manifestOptions: {
       background_color: '#F5F6FA'
